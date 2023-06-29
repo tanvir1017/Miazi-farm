1d304ba76b64e302e316d9252438f7bbf5ca5f55
tanvir1017 <tanvir.hossaindev1@gamil.com>
2023-06-29 19:51:34 +0600

    fix: 🐛 Solved error for can be escaped with &lsquo;
    Solved error for can be escaped with &lsquo;
    
    BREAKING CHANGE: 🧨 Solved error for can be escaped with &lsquo;
    
    ✅ Closes: Solved error for can be escaped with &lsquo;

--- components/shadecncomponents/userprofiledialog.tsx
+++ components/shadecncomponents/userprofiledialog.tsx
@@ -25,7 +25,7 @@ export function UserProfileDialog() {
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&lsquo;re done.
           </DialogDescription>
         </DialogHeader>
         <div className="grid gap-4 py-4">
